9d19cd3be7bd560d946310ef9369df3ed37769b9
The.D <18234905+The-D66@users.noreply.github.com>
2024-01-01 11:09:42 +0000

    change to secrets

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 # ff14House
+
 通过石之家社区的用户界面 , 判断玩家的房子是否快到期.
 
 房屋要到期时 , 房屋信息会出现（剩余xx天）的提示
 
-在房屋出现剩余天数提示时 , 脚本会通过电子邮件和server两种方式来通知你  
+在房屋出现剩余天数提示时 , 脚本会通过电子邮件和server两种方式来通知你
 
-这需要你填入完整的config
 # config相关内容
 
 ## 石之家
-使用一个账号登录石之家 , 获取cookie , 填入config , 
+
+使用一个账号登录石之家 , 获取cookie , 填入config ,
 
 之后尽量不要在别的地方登录这个账号 , 防止cookie被挤掉失效
 
 ## server酱
 
-打开server酱的网站<https://sct.ftqq.com/>
+打开server酱的网站[https://sct.ftqq.com/](https://sct.ftqq.com/)
 
 获取server的key填入config
 
 ## 电子邮件
+
 本人使用的是QQ邮箱 , 你可以选择使用其他邮箱
 
 填入发件人邮箱账户 , smtp授权码 , 收件人邮箱 , smtp服务器地址 , smtp服务器接口
@@ -30,14 +32,25 @@ QQ邮箱的smtp地址为: smtp.qq.com
 
 QQ邮箱的smtp接口为: 587
 
-## 自动运行
-可以使用我写好的workflow
+# 自动运行
 
-在上方的Action中找到名为daily的workflow
+自动运行采用 [GitHub Secrets](https://docs.github.com/zh/actions/security-guides/using-secrets-in-github-actions#creating-secrets-for-an-environment) 来保证信息安全，请到 Setting 中设置 Secrets 后
+
+再在上方的Action中找到名为daily的workflow
 
 即可每天八点钟运行一次
 
+需设置的 Secrets 包括（如果不需要其值可以为空，即 `''`，但必须设置）：
+
+|      名称      | 含义                                                               |
+| :-------------: | ------------------------------------------------------------------ |
+|      UUID      | 用户的uuid，多位以**空格**分隔：10001 10022 10003            |
+|     COOKIE     | 石之家Cookies，建议以单引号''包围，避免转义带来的问题，即'cookies' |
+|  SENDER_EMAIL  | 发件人邮箱                                                         |
+| SENDER_PASSWORD | 发件人邮箱密码，通常会需要提供单独生成的应用密码                   |
+| RECIPIENT_EMAIL | 收件人邮箱                                                         |
+|      PORT      | SMTP服务接口                                                       |
+|      SMTP      | SMTP服务器地址                                                     |
+|   SERVER_CHAN   | Server酱密钥                                                       |
+
 ![:name](https://count.getloli.com/get/@:WC-OP)
-
-
-
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import requests
 import json
 import configparser
+from typing import List
+from dataclasses import dataclass, field
+
+import requests
+import simple_parsing
+
 import notify
+
+
+@dataclass
+class Config:
+    uuid: List[int] = field(default_factory=list)
+    cookie: str = ''
+    serverChan: str = ''
+    sender_email: str = ''
+    sender_password: str = ''
+    recipient_email: str = ''
+    port: int = -1
+    smtp: str = ''
 
 
 def getHouseRemainDay(uuid, cookie):
@@ -12,6 +29,7 @@ def getHouseRemainDay(uuid, cookie):
         "https://apiff14risingstones.web.sdo.com/api/home/userInfo/getUserInfo",
         cookies=cookies,
         params=params,
+        timeout=40
     )
 
     data = json.loads(response.text)
@@ -19,16 +37,15 @@ def getHouseRemainDay(uuid, cookie):
     if data["code"] == 10000:
         if "house_remain_day" in data["data"]["characterDetail"][0]:
             info = (
-                "用户名:"
-                + data["data"]["characterDetail"][0]["character_name"]
-                + "到期时间:"
-                + data["data"]["characterDetail"][0]["house_remain_day"]
+                "用户名:" + data["data"]["characterDetail"][0]["character_name"] +
+                "到期时间:" + data["data"]["characterDetail"][0]["house_remain_day"]
             )
             print(info)
 
             try:
                 notify.send_email(
-                    sender_email, sender_password, recipient_email, smtp, port, info
+                    sender_email, sender_password, recipient_email, smtp, port,
+                    info
                 )
             except Exception as e:
                 print("发生异常:", e)
@@ -39,7 +56,9 @@ def getHouseRemainDay(uuid, cookie):
                 print("发生异常:", e)
 
         else:
-            print(data["data"]["characterDetail"][0]["character_name"] + "的房子没到期")
+            print(
+                data["data"]["characterDetail"][0]["character_name"] + "的房子没到期"
+            )
     else:
         info = "查询失败 , 请检查cookie是否过期"
         try:
@@ -55,25 +74,42 @@ def getHouseRemainDay(uuid, cookie):
             print("发生异常:", e)
 
 
-# 创建配置解析器对象
-config = configparser.RawConfigParser()
+parser = simple_parsing.ArgumentParser()
+parser.add_arguments(Config, dest="config")
+config = parser.parse_args().config
 
-# 读取配置文件
-config.read("config.ini", encoding="utf-8")
+if config.cookie != '':
+    # 石之家配置文件
+    uuid = config.uuid
+    cookie = config.cookie
+    # server酱的key
+    serverChen = config.serverChan
+    # 右键通知配置文件
+    sender_email = config.sender_email
+    sender_password = config.sender_password
+    recipient_email = config.recipient_email
+    smtp = config.smtp
+    port = config.port
+else:
+    # 创建配置解析器对象
+    config = configparser.RawConfigParser()
 
-# 获取配置项的值
+    # 读取配置文件
+    config.read("config.ini", encoding="utf-8")
 
-# 石之家配置文件
-uuid = eval(config.get("FF14", "uuid"))
-cookie = config.get("FF14", "cookie")
-# server酱的key
-serverChen = config.get("notifyServer", "serverChan")
-# 右键通知配置文件
-sender_email = config.get("notifyMail", "sender_email")
-sender_password = config.get("notifyMail", "sender_password")
-recipient_email = config.get("notifyMail", "recipient_email")
-smtp = config.get("notifyMail", "smtp")
-port = config.get("notifyMail", "port")
+    # 获取配置项的值
+
+    # 石之家配置文件
+    uuid = eval(config.get("FF14", "uuid"))
+    cookie = config.get("FF14", "cookie")
+    # server酱的key
+    serverChen = config.get("notifyServer", "serverChan")
+    # 右键通知配置文件
+    sender_email = config.get("notifyMail", "sender_email")
+    sender_password = config.get("notifyMail", "sender_password")
+    recipient_email = config.get("notifyMail", "recipient_email")
+    smtp = config.get("notifyMail", "smtp")
+    port = config.get("notifyMail", "port")
 
 for i in uuid:
     getHouseRemainDay(i, cookie)
